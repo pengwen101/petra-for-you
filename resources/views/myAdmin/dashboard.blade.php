d64003bdcfeac7f055934f015a5bffa671036e46
nicostap <nicoalexandrekurniawan@gmail.com>
2024-12-06 20:05:50 +0700

    Admin admin CRUD done

--- resources/views/myAdmin/dashboard.blade.php
+++ resources/views/myAdmin/dashboard.blade.php
@@ -1,5 +1,10 @@
 @extends('myAdmin.base')
 
 @section('contents')
-
+<div class="d-flex">
+    <div class="text-center items-center justify-center px-6 py-8 max-w-lg mx-auto">
+        <h1 class="text-4xl sm:text-5xl font-bold text-blue-600 mb-4">Welcome back Admin</h1>
+        <p class="text-lg sm:text-xl text-gray-600 mb-6">We're glad to have you back</p>
+    </div>
+</div>
 @endsection
